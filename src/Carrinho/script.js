a0e223d23ab424371d1274c9813553ba87ffcab5
Vicenzolevandoski <vicenzonogueira16@gmail.com>
2026-06-23 08:42:42 -0300

    Add files via upload

--- src/Carrinho/script.js
+++ src/Carrinho/script.js
@@ -1,15 +1,56 @@
 $(document).ready(function(){
-    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
-
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [
+    ]
     const listElement = $('#lista')
     const totalElement = $('#total')
 
     function exibirCarrinho(){
         listElement.empty()
-        let totalPreco = 0 
-
+        let totalPreco = 0
         $.each(carrinho, function(index, item){
-            const
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.valor.toFixed(2)}`)
+            const removeButton = $("button").text("❌").css("margin-left", "10px").click(function(){
+                removerItem(index)
+            })
+            listItem.append(removeButton)
+            listElement.append(listItem)
+
+            totalPreco += item.valor
         })
+        totalElement.text(`Total: $${totalPreco.toFixed(2)}`)
     }
+
+    function removerItem(index){
+        carrinho.splice(index, 1)
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+        exibirCarrinho()
+    }
+
+        exibirCarrinho()
 })
+
+function gerar(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listaElement.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+
+    const totalHtml = totalElement.innerHTML
+    const conteudoHTML = `
+        <html>
+            <head>
+                <meta charset="UTF-8">
+            </head>
+            <body>
+                <h1> PEDIDO CONFIRMADO</h1>
+                <h3> Agradecemos sua compra e sua preferencia.</h3>
+                <br>
+                $(listaHtml)
+                <br>
+                <br>
+                $(totalHtml)
+            </body>
+        </html>
+    `
+}
